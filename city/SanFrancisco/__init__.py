e4b478cf40eab596b1ac5d2dab82d409a3e13362
nkfx <canin@dreamfreely.org>
2020-06-06 14:11:19 -0500

    updated SF & Oakland inits

--- city/SanFrancisco/__init__.py
+++ city/SanFrancisco/__init__.py
@@ -22,13 +22,14 @@ class Sanfrancisco(Jurisdiction):
         #REQUIRED: define an organization using this format
         #where org_name is something like Seattle City Council
         #and classification is described here:
-        org = Organization(name="org_name", classification="legislature")
+        city = Organization('City of San Francisco', classification='executive')
+        yield city
 
         # OPTIONAL: add posts to your organizaion using this format,
         # where label is a human-readable description of the post (eg "Ward 8 councilmember")
         # and role is the position type (eg councilmember, alderman, mayor...)
         # skip entirely if you're not writing a people scraper.
-        org.add_post(label="position_description", role="position_type")
+        # org.add_post(label="position_description", role="position_type")
 
         #REQUIRED: yield the organization
-        yield org
+        # yield org
