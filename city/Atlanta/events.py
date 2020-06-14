ebd81e929583327c190ab3e19c23f4de0f17dfdc
nkfx <canin@dreamfreely.org>
2020-06-13 21:17:04 -0500

    Atlanta works, needs location still

--- city/Atlanta/events.py
+++ city/Atlanta/events.py
@@ -22,14 +22,15 @@ import pytz
 
 tz = pytz.timezone("US/Eastern")
 
-TIME_FORMAT = '%b %d, %Y %I:%M %p'
+TIME_FORMAT = '%B %d, %Y %I:%M %p'
+TIME_FORMAT2 = '%B %d, %Y'
 
 # Set initial variables for City, etc
 calendar_url = 'https://citycouncil.atlantaga.gov/other/events/public-meetings'
 
-nope_calendar_url = 'https://www.lacity.org/government/meeting-calendars/council-and-committee-meetings'
+base_url = 'https://citycouncil.atlantaga.gov'
 
-
+os.system('pkill Xvfb')
 start_cmd = "Xvfb :91 && export DISPLAY=:91 &"
 xvfb = Xvfb()
 
@@ -39,35 +40,43 @@ xvfb.start()
 print("started Xvfb")
 
 # Initiate and start the Browser
-br = wd.Chrome()
-# br = wd.Firefox()
+# br = wd.Chrome()
+br = wd.Firefox()
 
 EVENTS = []
 NEXTLINK = ''
 
+def getSite(url):
+    br.get(url)
+    sleep(5)
+    s = br.page_source
+    b = html.fromstring(s)
+    return b
 
 def getEvents(url):
     global NEXTLINK
-    br.get(url)
-    sleep(5)
-    month, year = br.find_elements_by_class_name('calendar_title_content')[0].text.split(' ')
-    NEXTLINK = br.find_elements_by_class_name('calendar_nextprev')[1].find_element_by_xpath('.//a').get_attribute('href')
-    days = br.find_elements_by_class_name('calendar_day_with_items')
+    b = getSite(url)
+    month, year = b.xpath('.//*/td[@class="calendar_title_content"]/h2/text()')[0].strip().split(' ')
+    NEXTLINK = b.xpath('.//*/td[@class="calendar_nextprev"]/a/@href')[1]
+    days = b.xpath('.//*/td[@class="calendar_day calendar_day_with_items"]')
     for day in days:
-        date = '{0} {1}, {2}'.format(month, day.text, year)
-        events = day.find_elements_by_class_name('calendar_item')
+        date = '{0} {1}, {2}'.format(month, day.xpath('.//text()')[0].strip(), year)
+        events = day.xpath('.//div/div[@class="calendar_item"]')
         for event in events:
             d = {}
-            time = event.find_element_by_class_name('calendar_eventtime').text
-            d['title'] = event.find_element_by_class_name('calendar_eventlink').get_attribute('title')
-            d['link'] = event.find_element_by_class_name('calendar_eventlink').get_attribute('href')
-            dateTime = '{0} {1}'.format(date, time)
-            d['datetime'] = datetime.strptime(dateTime, TIME_FORMAT)
+            d['title'] = event.xpath('.//a/text()')[0]
+            d['link'] = event.xpath('..//a/@href')[0]
+            try:
+                time = event.xpath('.//span/text()')[0]
+                dateTime = '{0} {1}'.format(date, time)
+                d['datetime'] = datetime.strptime(dateTime, TIME_FORMAT)
+            except Exception as e:
+                d['datetime'] = datetime.strptime(date, TIME_FORMAT2)
             EVENTS.append(d)
 
 
 getEvents(calendar_url)
-getEvents(NEXTLINK)
+getEvents(base_url + NEXTLINK)
 
 EVENTS = [i for n, i in enumerate(EVENTS) if i not in EVENTS[n + 1:]] 
 
